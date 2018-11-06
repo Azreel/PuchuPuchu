ae4f6a212675dd939a5f44d8f10fc8c67c85c825
olives0415 <s16t255@stu.kagawa-u.ac.jp>
2018-11-06 18:01:27 +0900

    Puchu Class & PuchuPair Class の設定(初期段階)

--- src/Puchu.java
+++ src/Puchu.java
@@ -1,4 +1,33 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class Puchu {
-
+	
+	public static final int Obs = -1;
+	public static final int Empty = 0;
+	public static final int Pat1 = 1;
+	public static final int Pat2 = 2;
+	public static final int Pat3 = 3;
+	public static final int Pat4 = 4;
+	public static final int Pat5 = 5;
+	public static final int Pat6 = 6;
+	public static final int Pat7 = 7;
+	
+	public int type;
+	public Image face;
+	public int x;
+	public int y;
+	public int id;
+	
+	public Puchu(int set_type, int set_x, int set_y, int set_id) {
+		this.type = set_type;
+		SetFace(set_type);
+		this.x = set_x;
+		this.y = set_y;
+		this.id = set_id;
+	}
+	
+	void SetFace(int img_type) {
+		//this.face = Toolkit.getDefaultToolkit().getImage(getClass().getResource("images/face"+img_type)); 
+	}
 }
--- src/PuchuPair.java
+++ src/PuchuPair.java
@@ -1,4 +1,41 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class PuchuPair {
-
+	public static final int Ver_Up = 0;
+	public static final int Hor_Right = 1;
+	public static final int Ver_Bottom = 2;
+	public static final int Hor_Left = 3;
+	
+	public static final int Status_Now = 0;
+	public static final int Status_Next1 = 1;
+	public static final int Status_Next2 = 2;
+	
+	Puchu puchu1;
+	Puchu puchu2;
+	int _x;
+	int _y;
+	public int form;
+	
+	//PuchuPair pp = new PuchuPair(0, PuchuPair.Status_Now);
+	
+	public PuchuPair(int set_id, int status){
+		switch(status) {
+			case PuchuPair.Status_Now : _x = 10; _y = 10; break;
+			case PuchuPair.Status_Next1 : _x = 2; _y = 2; break;
+			case PuchuPair.Status_Next2 : _x = 1; _y = 1; break;
+		}
+		puchu1 = new Puchu((int)(Math.random()*7)+1, _x, _y, set_id);
+		puchu2 = new Puchu((int)(Math.random()*7)+1, _x, _y, set_id + 1);
+		//System.out.println(puchu1.type);
+		//System.out.println(puchu2.type);
+	}
+	
+	public void TurnRight() {
+		
+	}
+	
+	public void TurnLeft() {
+		
+	}
 }
