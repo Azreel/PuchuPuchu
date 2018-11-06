b9f43a061fc8040b0d9b61b945dc1e3636c348cc
Marron Akanishi <marron.general@gmail.com>
2018-11-06 18:22:45 +0900

    Title途中

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,4 +1,35 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class GameMain {
-
+	final int ScreenW = 960;
+	final int ScreenH = 640;
+	
+	public JFrame frame = new JFrame();
+	
+	private Field me;
+	private Field rival;
+	private Title title;
+	
+	public GameMain(){
+		MakeWindow();
+		title = new Title();
+		title.setPreferredSize(new Dimension(ScreenW, ScreenH));
+		frame.add(title);
+		frame.pack();
+	}
+	
+	private void MakeWindow() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 終了処理
+		frame.setSize(ScreenW, ScreenH);
+	    frame.setLocationRelativeTo(null);
+		frame.setTitle("ぷちゅぷちゅ");
+		frame.setResizable(false);// サイズ変更不可
+		frame.setLayout(new GridLayout());
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new GameMain();
+	}
 }
--- src/Title.java
+++ src/Title.java
@@ -1,4 +1,29 @@
 
-public class Title {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
+public class Title extends JPanel{
+	Toolkit tk;
+	JButton solo;
+	JButton duo;
+	JLabel myIP;
+	Image bg;
+	
+	public Title() {
+		solo = new JButton("1Pプレイ");
+        duo = new JButton("2Pプレイ");
+        tk = Toolkit.getDefaultToolkit(); 
+        bg = tk.getImage(getClass().getResource("title.png"));
+        
+        this.add(solo);
+        this.add(duo);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.drawImage(bg, null, this);
+    }
 }
