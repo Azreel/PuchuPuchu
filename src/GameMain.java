33ed05b41eb82ed54ed24a922835e7adc5b7b340
Marron Akanishi <marron.general@gmail.com>
2018-11-07 01:49:22 +0900

    必要最低限の表示

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,22 +1,20 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class GameMain {
+public class GameMain extends Thread {
 	final int ScreenW = 960;
 	final int ScreenH = 640;
 	
-	public JFrame frame = new JFrame();
+	public int gameStatus = 0;
 	
-	private Field me;
-	private Field rival;
-	private Title title;
+	private JFrame frame = new JFrame();
+	private long loopDelay = 16;
+	private Field me = null;
+	private Field rival = null;
+	private Title title = null;
 	
 	public GameMain(){
 		MakeWindow();
-		title = new Title();
-		title.setPreferredSize(new Dimension(ScreenW, ScreenH));
-		frame.add(title);
-		frame.pack();
 	}
 	
 	private void MakeWindow() {
@@ -29,7 +27,55 @@ public class GameMain {
 		frame.setVisible(true);
 	}
 	
+	public void run() {
+		while(true) {
+			// 60fps保つ
+			try {
+				if(loopDelay > 0) {
+					sleep(loopDelay);
+				}
+			}catch(Exception e) {}
+			// ゲーム全体の管理
+			long start = System.currentTimeMillis();
+			switch(gameStatus) {
+			case 0: // タイトル
+				if(title == null) {
+					System.out.println("タイトル生成");
+					me = null;
+					rival = null;
+					title = new Title(this);
+					title.setPreferredSize(new Dimension(ScreenW, ScreenH));
+					frame.add(title);
+					frame.pack();
+				}
+				break;
+			case 1: // 1Pプレイ
+				if(me == null) {
+					System.out.println("1Pモード");
+					frame.remove(title);
+					title = null;
+					me = new Field();
+				}
+				break;
+			case 2: // 2Pプレイ
+				if(me == null && rival == null) {
+					System.out.println("1Pモード");
+					frame.remove(title);
+					title = null;
+					me = new Field();
+					rival = new Field();
+				}
+				break;
+			default: // エラー
+				System.out.println("予期せぬゲームステータス");
+				break;
+			}
+			loopDelay = 16 - (System.currentTimeMillis() - start);
+		}
+	}
+	
 	public static void main(String[] args) {
-		new GameMain();
+		GameMain gm = new GameMain();
+		gm.start();
 	}
 }
--- src/Title.java
+++ src/Title.java
@@ -1,24 +1,76 @@
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Title extends JPanel{
-	Toolkit tk;
-	JButton solo;
-	JButton duo;
-	JLabel myIP;
-	Image bg;
+	final Toolkit tk = Toolkit.getDefaultToolkit();
 	
-	public Title() {
-		solo = new JButton("1Pプレイ");
-        duo = new JButton("2Pプレイ");
-        tk = Toolkit.getDefaultToolkit(); 
-        bg = tk.getImage(getClass().getResource("title.png"));
+	GameMain gm;
+	Image bg;
+	JButton soloBtn, duoBtn;
+	JTextField rivalIP;
+	JLabel rival, myIP;
+	Font btnFont = new Font("MS ゴシック", Font.PLAIN, 24);
+	Font labelFont = new Font("MS ゴシック", Font.PLAIN, 20);
+	
+	public Title(GameMain parent) {
+		gm = parent;
+		this.setLayout(null);
+		
+		//背景画像
+		bg = tk.getImage(getClass().getResource("title.png"));
+		//自分のIPアドレス
+        myIP = new JLabel(GetIPaddr());
+        myIP.setBounds(225, 470, 400, 30);
+        myIP.setFont(labelFont);
+		//1Pプレイ
+		soloBtn = new JButton("1Pプレイ");
+		soloBtn.setBounds(570, 450, 160, 60);
+		soloBtn.setFont(btnFont);
+		soloBtn.addActionListener(new SoloPlayBtn());
+		//IPアドレス入力欄
+		rival = new JLabel("相手のIPアドレス:");
+		rival.setBounds(225, 550, 300, 30);
+		rival.setFont(labelFont);
+		rivalIP = new JTextField("0.0.0.0");
+		rivalIP.setBounds(400, 550, 160, 30);
+		rivalIP.setFont(labelFont);
+        //2Pプレイ
+		duoBtn = new JButton("2Pプレイ");
+		duoBtn.setBounds(570, 530, 160, 60);
+		duoBtn.setFont(btnFont);
+		soloBtn.addActionListener(new DuoPlayBtn());
         
-        this.add(solo);
-        this.add(duo);
+        this.add(soloBtn);
+        this.add(duoBtn);
+        this.add(rival);
+        this.add(rivalIP);
+        this.add(myIP);
     }
+	
+	private String GetIPaddr() {
+		try {
+			InetAddress addr = InetAddress.getLocalHost();
+			return "自分のIPアドレス: " + addr.getHostAddress();
+		} catch(UnknownHostException e) {
+			return "ネットワークに接続されていません";
+		}
+	}
+	
+	private class SoloPlayBtn implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			gm.gameStatus = 1;
+		}
+	}
+	
+	private class DuoPlayBtn implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			gm.gameStatus = 2;
+		}
+	}
 
     @Override
     public void paintComponent(Graphics g) {
