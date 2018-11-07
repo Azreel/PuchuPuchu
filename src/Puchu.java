9d9884e3db13d653c251831ba5cd8dc458ecd112
olives0415 <s16t255@stu.kagawa-u.ac.jp>
2018-11-07 13:13:28 +0900

    コメントアウト追加

--- src/Puchu.java
+++ src/Puchu.java
@@ -3,15 +3,18 @@ import javax.swing.*;
 
 public class Puchu {
 	
-	public static enum Type { Obs, Emp, Pat1, Pat2, Pat3, Pat4, Pat5, Pat6, Pat7 }
+	public static enum Type { Obs, Emp, Pat1, Pat2, Pat3, Pat4, Pat5, Pat6 };
+	// Obs : お邪魔
+	// Emp : ぷちゅなし
+	// Pat1 ~ Pat6
 	
-	public int puchu_type;
-	public Image face;
-	public int x;
-	public int y;
-	public int id;
+	public Type puchu_type;	// ぷちゅの種類
+	public Image face;		// ぷちゅの顔
+	public int x;			// 座標
+	public int y;			//
+	public int id;			// 1次元配列管理上のID
 	
-	public Puchu(int set_type, int set_x, int set_y, int set_id) {
+	public Puchu(Type set_type, int set_x, int set_y, int set_id) {
 		this.puchu_type = set_type;
 		SetFace(set_type);
 		this.x = set_x;
@@ -19,7 +22,7 @@ public class Puchu {
 		this.id = set_id;
 	}
 	
-	void SetFace(int img_type) {
-		//this.face = Toolkit.getDefaultToolkit().getImage(getClass().getResource("images/face"+img_type)); 
+	void SetFace(Type img_type) {
+		//this.face = Toolkit.getDefaultToolkit().getImage(getClass().getResource("images/face"+img_type.values())); 
 	}
 }
--- src/PuchuPair.java
+++ src/PuchuPair.java
@@ -9,24 +9,19 @@ public class PuchuPair {
 	
 	Puchu puchu1;
 	Puchu puchu2;
-	int _x = 200;
-	int _y = 200; 
-	public int form;
+	int _x = 200;		// 生成座標
+	int _y = 200; 		//
+	public int form;	// 姿勢状態(ぷちゅ1を中心にぷちゅ2がどこにいるか)
 	
 	public PuchuPair(int set_id){
 		this.form = PuchuPair.Up;
-		this.puchu1 = new Puchu((int)(Math.random()*7)+1, _x, _y, set_id);
-		this.puchu2 = new Puchu((int)(Math.random()*7)+1, _x, _y - 40, set_id + 1);
+		this.puchu1 = new Puchu(Puchu.Type.valueOf("Pat"+(int)(Math.random()*6)+1), _x, _y, set_id);
+		this.puchu2 = new Puchu(Puchu.Type.valueOf("Pat"+(int)(Math.random()*6)+1), _x, _y - 40, set_id + 1);
 		//System.out.println(puchu1.type);
 		//System.out.println(puchu2.type);
 	}
 	
-//	-1 : p/2
-//	 0 : 0
-//	 1: -p/2
-//	 2: -p
-	
-	//--
+	//-- ぷちゅの回転処理
 	public void TurnRight() {
 		puchu2.x = puchu1.x + (int)(40 * Math.cos((form - 1) * -Math.PI/2));	// 
 		puchu2.y = puchu1.y + (int)(40 * Math.sin((form - 1) * -Math.PI/2));
